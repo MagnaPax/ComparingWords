e6a271a3008d42746a0715a5803530c6e8321d72
MagnaPax <MagnaPax@outlook.kr>
2020-04-13 17:03:06 +0900

    프로젝트 생성

---
+++ ComparingWords/ComparingWords/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ComparingWords
+//
+//  Created by 한천희 on 2020/04/13.
+//  Copyright © 2020 한천희. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

c52822a8b007b4cee60f3e60063a3c80f7ed2baf
MagnaPax <MagnaPax@outlook.kr>
2020-04-13 18:36:34 +0900

    오브젝트들 코드와 연결

--- ComparingWords/ComparingWords/ViewController.swift
+++ ComparingWords/ComparingWords/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: Properties
+    @IBOutlet weak var currentWordLabel: UILabel!
+    @IBOutlet weak var nextWordLabel: UILabel!
+    @IBOutlet weak var wordInputTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    //MARK: Actions
+    @IBAction func inputButton(_ sender: UIButton) {
+    }
+    
+    
 }
 

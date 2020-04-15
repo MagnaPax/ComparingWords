f0c94ce672e299bd223fbe28351f18543b99e178
MagnaPax <MagnaPax@outlook.kr>
2020-04-15 19:04:19 +0900

    [#7] feat : 틀렸을 때 Label에 border 테두리를 빨간 색으로 바꿈.
    text field 와 현재 label 이 같지 않으면 text field 의 테두리를 빨간 색으로 바꿈
    두 개가 같으면 다시 투명하게 만듦
    반복되는 코드가 너무 많다.

--- ComparingWords/ComparingWords/ViewController.swift
+++ ComparingWords/ComparingWords/ViewController.swift
@@ -53,12 +53,19 @@ class ViewController: UIViewController, UITextFieldDelegate {
             // 현재 단어를 다음 단어로 바꾼 뒤 새로운 다음 단어를 받음
             let randomWords = returnRandomWords(fromList: wordList)
             nextWord.text! = randomWords.nextWord
+            
+            // text field 색깔을 원래상태인 clear로 돌림
+            typedWord.layer.borderColor = UIColor.clear.cgColor
+            
         }
-        
+        else {
+            // text field 의 색깔을 바꿈
+            typedWord.layer.borderColor = UIColor.red.cgColor
+            typedWord.layer.borderWidth = 1.0
+        }
         
         // text field 지우기
         clearTextField(textField: typedWord)
-                
     }
     
     
@@ -109,7 +116,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-                // 두 아규먼트(현재 label, 입력 text field) 같으면 참
+        
+        // 두 아규먼트(현재 label, 입력 text field) 같으면 참
         let areTheseSame = compareWords(wordOnLabel: currentWord, typedTextField: typedWord)
         
         // 입력과 현재단어가 같으면 다음 단어를 현재 단어로 옮김
@@ -119,8 +127,16 @@ class ViewController: UIViewController, UITextFieldDelegate {
             // 현재 단어를 다음 단어로 바꾼 뒤 새로운 다음 단어를 받음
             let randomWords = returnRandomWords(fromList: wordList)
             nextWord.text! = randomWords.nextWord
+            
+            // text field 색깔을 원래상태인 clear로 돌림
+            typedWord.layer.borderColor = UIColor.clear.cgColor
+            
         }
-        
+        else {
+            // text field 의 색깔을 바꿈
+            typedWord.layer.borderColor = UIColor.red.cgColor
+            typedWord.layer.borderWidth = 1.0
+        }
         
         // text field 지우기
         clearTextField(textField: typedWord)

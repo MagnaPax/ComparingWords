2f960b3292f8b69ff28cfe625910a55f6c8d7614
MagnaPax <MagnaPax@outlook.kr>
2020-04-16 14:20:42 +0900

    [#7] refactor : 중복 코드 리팩토링
    확인 버튼을 눌렀을 때와 리턴을 눌렀을 때 처리해주는 동일한 동작들을 메소드 추출로 하나로 묶음

--- ComparingWords/ComparingWords/ViewController.swift
+++ ComparingWords/ComparingWords/ViewController.swift
@@ -42,30 +42,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     //MARK: Actions
     @IBAction func completeButton(_ sender: UIButton) {
-        
-        // 두 아규먼트(현재 label, 입력 text field) 같으면 참
-        let areTheseSame = compareWords(wordOnLabel: currentWord, typedTextField: typedWord)
-        
-        // 입력과 현재단어가 같으면 다음 단어를 현재 단어로 옮김
-        if areTheseSame {
-            replaceWord(thisWord: currentWord, toWord: nextWord)
-            
-            // 현재 단어를 다음 단어로 바꾼 뒤 새로운 다음 단어를 받음
-            let randomWords = returnRandomWords(fromList: wordList)
-            nextWord.text! = randomWords.nextWord
-            
-            // text field 색깔을 원래상태인 clear로 돌림
-            typedWord.layer.borderColor = UIColor.clear.cgColor
-            
-        }
-        else {
-            // text field 의 색깔을 바꿈
-            typedWord.layer.borderColor = UIColor.red.cgColor
-            typedWord.layer.borderWidth = 1.0
-        }
-        
-        // text field 지우기
-        clearTextField(textField: typedWord)
+        processWordComparingProcessing()
     }
     
     
@@ -116,6 +93,13 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        processWordComparingProcessing()
+        return true
+    }
+    
+ 
+    // 단어를 비교하고 처리해줌
+    func processWordComparingProcessing() {
         
         // 두 아규먼트(현재 label, 입력 text field) 같으면 참
         let areTheseSame = compareWords(wordOnLabel: currentWord, typedTextField: typedWord)
@@ -140,8 +124,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         // text field 지우기
         clearTextField(textField: typedWord)
-        
-        return true
     }
     
     
